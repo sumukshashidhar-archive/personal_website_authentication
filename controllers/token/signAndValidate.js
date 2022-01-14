b24090ae39e017b42fe73b4c3b4995c40a6b531a
sumukshashidhar <sumukuuu@gmail.com>
2022-01-14 11:13:54 -0800

    feat[jwt]: fix typo

--- controllers/token/signAndValidate.js
+++ controllers/token/signAndValidate.js
@@ -16,55 +16,55 @@ const publicKEY = fs.readFileSync(publicKeyPath, "utf-8");
 const privateKEY = fs.readFileSync(privateKeyPath, "utf-8");
 
 module.exports = {
-  sign: function (userObject) {
-    /*
-     *    Signing function that takes in a user object with 3 fields. email, role and data,
-     *    and returns a signed JWT token
-     */
-    return new Promise(function (resolve, reject) {
-      logger.silly(`Function JWT signer called for userobject ${userObject}`);
-      jwt.sign(
-        {
-          email: userObject["email"],
-          role: userObject["role"],
-          data: userObject["data"],
-        },
-        privateKEY,
-        tokenOptions.signOptions,
-        function (error, signedToken) {
-          if (error) {
-            logger.error(`Error in JWT Signer: ${error}`);
-            reject(error);
-          } else {
-            logger.silly(`Returning ${signedToken}`);
-            resolve(signedToken);
-          }
-        }
-      );
-    });
-  },
+    sign: function (userObject) {
+        /*
+         *    Signing function that takes in a user object with 3 fields. email, role and data,
+         *    and returns a signed JWT token
+         */
+        return new Promise(function (resolve, reject) {
+            logger.silly(`Function JWT signer called for user-object ${userObject}`);
+            jwt.sign(
+                {
+                    email: userObject["email"],
+                    role: userObject["role"],
+                    data: userObject["data"],
+                },
+                privateKEY,
+                tokenOptions.signOptions,
+                function (error, signedToken) {
+                    if (error) {
+                        logger.error(`Error in JWT Signer: ${error}`);
+                        reject(error);
+                    } else {
+                        logger.silly(`Returning ${signedToken}`);
+                        resolve(signedToken);
+                    }
+                }
+            );
+        });
+    },
 
-  validate: function (token) {
-    /*
-     *  Validation function that takes in a JSON Web Token, and validates it, returning a decoded object
-     *  if it is valid
-     */
-    return new Promise(function (resolve, reject) {
-      logger.silly(`Function JWT validator called for token ${token}`);
-      jwt.verify(
-        token,
-        publicKEY,
-        tokenOptions.verifyOptions,
-        function (error, decodedToken) {
-          if (error) {
-            logger.error(`Error in JWT validator: ${error}`);
-            reject(error);
-          } else {
-            logger.silly(`Decoded token into ${decodedToken}`);
-            resolve(decodedToken);
-          }
-        }
-      );
-    });
-  },
+    validate: function (token) {
+        /*
+         *  Validation function that takes in a JSON Web Token, and validates it, returning a decoded object
+         *  if it is valid
+         */
+        return new Promise(function (resolve, reject) {
+            logger.silly(`Function JWT validator called for token ${token}`);
+            jwt.verify(
+                token,
+                publicKEY,
+                tokenOptions.verifyOptions,
+                function (error, decodedToken) {
+                    if (error) {
+                        logger.error(`Error in JWT validator: ${error}`);
+                        reject(error);
+                    } else {
+                        logger.silly(`Decoded token into ${decodedToken}`);
+                        resolve(decodedToken);
+                    }
+                }
+            );
+        });
+    },
 };
